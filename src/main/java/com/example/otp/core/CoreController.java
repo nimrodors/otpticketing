6e0d35d6af61616a3c52e56609368b461a2e12ef
Nimrod <nimrodors@gmail.com>
2025-08-06 22:10:04 +0200

    Add annotation to Core Controller

--- src/main/java/com/example/otp/core/CoreController.java
+++ src/main/java/com/example/otp/core/CoreController.java
@@ -1,7 +1,10 @@
 package com.example.otp.core;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
+@RequestMapping("/core")
 public class CoreController {
 }
